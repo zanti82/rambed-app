15670798ce7c5d448d4b2c17e717fae19b8588da
zanti82 <zanti82@gmail.com>
2026-05-31 14:15:34 -0500

    fix: agregar PATCH a CORS config

--- backend/Rambed360/src/main/java/Rambed360/config/CorsConfig.java
+++ backend/Rambed360/src/main/java/Rambed360/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // en origin ponemos la url de railway
                         .allowedOrigins("http://localhost:5173",  "https://rambed-app.vercel.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
 
